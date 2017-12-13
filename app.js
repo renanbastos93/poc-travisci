ef5bf51342fd120571c5f306855645922c38fcd8
renanbastos93 <renanbastos.tec@gmail.com>
2017-12-13 00:02:37 -0200

    chaged use to get

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-require('express')()
-.use('/', (req,res)=>res.status(200).send("Hello Word!"))
+module.exports = require('express')()
+.get('/', (req,res)=>res.status(200).send("Hello Word!"))
+.get('/api', (req, res)=>res.status(200).json({message: "voce está na api!"}))
 .listen(process.env.PORT||3000, console.log(`Ligou ${process.env.PORT}`));
