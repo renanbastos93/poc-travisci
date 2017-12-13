24498ffe0aa0ef0a4abdb110e6203fbabef08933
renanbastos93 <renanbastos.tec@gmail.com>
2017-12-13 00:02:59 -0200

    improvement test for API

--- test/index.js
+++ test/index.js
@@ -1,7 +1,35 @@
-const assert = require('assert')
+let chai = require('chai');
+let expect = require('chai').expect;
+let server = require('../app');
+let chaiHttp = require('chai-http');
 
-describe("Iniciando testes",()=>{
-    it("testando se 1 é iqual a 1", ()=>{
-        assert.equal(1,1);
+chai.use(chaiHttp);
+
+
+
+describe("POC TRAVIS-CI TESTs API",()=>{
+    before(function(){
+        // console.log("Iniciando testes");
+    });
+    after(function(){
+        // console.log("Finalizando testes");
+        setTimeout(process.exit, 1500);
+    });
+    it("GET /", ()=>{
+        chai.request(server)
+        .get('/')
+        .end((err, res) => {
+            expect(res.text).to.be.an('string');
+            expect(res).to.have.status(200);
+        });
+    });
+    it("GET /api", ()=>{
+        chai.request(server)
+        .get('/api')
+        .end((err, res) => {
+            expect(res.body).to.be.an('object');
+            expect(res.body).to.have.a.property('message');
+            expect(res).to.have.status(200);
+        });
     });
 });
